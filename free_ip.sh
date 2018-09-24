b1ce9e7e596122d3621568bb452a6b6a687069fb
Kevin Klein <git@kevin1904.de>
2018-09-24 15:26:53 +0200

    Changes to be committed: 	modified:   free_ip.sh 	modified:   get_ip.sh

--- free_ip.sh
+++ free_ip.sh
@@ -2,19 +2,21 @@
 
 folder=`pwd`
 
+# add get_ip.sh as function
 function get_free(){
 	$folder/get_ip.sh $1
 }
 
 while [ 1 > 0 ]
 do
-ip=`shuf -n 1 free_ip`
+ip=`shuf -n 1 free_ip` #outputs random IP from free_ip list
 
 { # add "#" to debug
-get_free $ip
+get_free $ip #check IP again for PTR
 }&> /dev/null # add "#" to debug
 TEST=$?
 
+# minimal output
 if [ $TEST -eq 0 ]; then
 	echo $ip
 	exit 0
--- get_ip.sh
+++ get_ip.sh
@@ -6,10 +6,12 @@ file=$folder/free_ip
 networks=$folder/networks
 date=`date -r $file`
 
+# first time creation; first scan
 if [ ! -f $file ];then
 nmap -v -sn -iL $networks -oG - | awk '/Down$/{print $2}' > $file
 fi
 
+# if last scan older 5 minutes - reduces network latency in large environments
 if test `find $file -mmin +5`
 then
 	echo "New scan started"
@@ -18,19 +20,20 @@ else
 	echo "Last scan: $date"
 fi
 
+# fast search from file, instead of scanning everytime
 ip=`cat $file | grep -w $1`
 
 if [ -z "$ip" ];then
 echo "IP $1 is in use:"
 resolveip -s $1
-exit 2
+exit 2 #exit code used in free_ip.sh
 fi
 
 free=`resolveip -s $ip`
 
 if [ -z "$free" ];then
 	echo "IP is free"
-	exit 0
+	exit 0 #exit code used in free_ip.sh
 else
 	echo "IP is free, but has DNS: $free"
 	echo "skipping IP"
